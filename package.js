f82d0dd168fe6e54a4c6d22b838f6b5261bb5af1
Timothy Pfafman <tim@pfafman.com>
2016-03-17 07:39:24 -0700

    switch to crossfilter2

--- package.js
+++ package.js
@@ -1,11 +1,15 @@
 Package.describe({
   name: 'pfafman:crossfilter',
   summary: "Fast n-dimensional filtering and grouping of records.",
-  version: "1.3.12_1",
+  version: "2.0.0_3",
   git: "https://github.com/pfafman/meteor-crossfilter.git"
 });
 
+// Npm.depends({
+//   "crossfilter2": "2.0.0"
+// });
+
 Package.on_use(function (api, where) {
   api.versionsFrom("METEOR@1.0");
-  api.add_files('lib/crossfilter.js', ['client', 'server']);
+  api.add_files('lib/crossfilter.js', ['client']);
 });
