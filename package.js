b6735b588a2ce7270dbf84e0fffc7581dd1d6686
Timothy Pfafman <tim@pfafman.com>
2016-04-03 06:43:39 -0700

    but 1.3 should be pulled via npm

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'pfafman:crossfilter',
   summary: "Fast n-dimensional filtering and grouping of records.",
-  version: "1.4.0_1",
+  version: "1.4.0_2",
   git: "https://github.com/pfafman/meteor-crossfilter.git"
 });
 
@@ -10,7 +10,7 @@ Package.describe({
 // });
 
 Package.on_use(function (api, where) {
-  api.versionsFrom("METEOR@1.0");
+  api.versionsFrom("METEOR@1.3");
   api.use("modules");
   api.add_files('lib/crossfilter.js', ['client', 'server']);
 });
